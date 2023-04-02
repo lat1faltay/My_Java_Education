f18c216a8168a9117456a14035398281ca107915
Latif Altay <latifaltay00@gmail.com>
2023-04-02 23:19:30 +0300

    02.04.2023_commit2

--- 40-Checked_Exception/readingFileDemo/src/Main.java
+++ 40-Checked_Exception/readingFileDemo/src/Main.java
@@ -6,10 +6,18 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = null;
+        int total = 0;
         try {
              reader = new BufferedReader(new FileReader("C:\\java_edu\\40-Checked_Exception\\readingFileDemo\\src\\sayilar.txt"));
+             String line = null;
+             while((line = reader.readLine()) != null){
+                 total += Integer.valueOf(line);
+             }
+            System.out.println("Toplam = " + total);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        }catch (Exception exception){
+            exception.printStackTrace();
         }finally {
             reader.close();
         }
