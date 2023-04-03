10a284d632f08599fc35f4d1d7427dee5e8895d1
Latif Altay <latifaltay00@gmail.com>
2023-04-03 17:33:01 +0300

    03.04.2023_2

--- 42-WorkingWithFiles/workingWithFiles/src/Main.java
+++ 42-WorkingWithFiles/workingWithFiles/src/Main.java
@@ -3,7 +3,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        getFileInfo();
+    }
 
+
+    public static void createFile(){
         File file = new File("C:\\java_edu\\files\\students.txt");
 
         try{
@@ -16,8 +20,19 @@ public class Main {
         }catch (IOException exception){
             exception.printStackTrace();
         }
-
-
-
     }
+
+    public static void getFileInfo(){
+        File file = new File("C:\\java_edu\\files\\students.txt");
+        if(file.exists()){
+            System.out.println("Dosya adı: " +file.getName());
+            System.out.println("Dosya yolu: " + file.getAbsoluteFile());
+            System.out.println("Dosya yazılabilir mi: " + file.canWrite());
+            System.out.println("Dosya okunabilir mi: " + file.canRead());
+            System.out.println("Dosya yazılabilir mi: " + file.canExecute());
+            System.out.println("Dosya boyutu: " + file.length());
+        }
+    }
+
+
 }
