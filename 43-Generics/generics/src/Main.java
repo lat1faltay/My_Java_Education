353eeab05904fb5bbb7c2afb280125451d77266a
Latif Altay <latifaltay00@gmail.com>
2023-04-04 03:36:12 +0300

    04.04.2023_2

---
+++ 43-Generics/generics/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        ArrayList<String> sehirler = new ArrayList<String>();
+        sehirler.add("İstanbul");
+        sehirler.add("Ankara");
+        sehirler.add("İzmir");
+
+        // Customer
+        // Product
+
+
+        // Validator
+        */
+
+        MyList<Customer> sehirler = new MyList<Customer>();
+        sehirler.add(new Customer());
+
+
+
+    }
+}
