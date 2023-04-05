528ec50da44d861a2e35df9b990d54407ba7aef2
Latif Altay <latifaltay00@gmail.com>
2023-04-05 06:15:38 +0300

    db_islemleri_2

--- 46-JDBC_Intro/jdbcIntro/src/Main.java
+++ 46-JDBC_Intro/jdbcIntro/src/Main.java
@@ -1,5 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-          
+
+    static String userName = "root";
+    static String password = "Deneme01!";
+    static String dbUrl = "jdbc:mysql://localhost:3306/world";
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = null;
+
+        try{
+           connection = DriverManager.getConnection(dbUrl,userName,password);
+            System.out.println("Bağlantı başarılı");
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }finally {
+            connection.close();
+            System.out.println("Bağlantı bitti");
+        }
+
     }
 }
