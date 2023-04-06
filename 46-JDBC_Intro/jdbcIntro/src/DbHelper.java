567411cc2f82c8f7cc627e8492d5d2f9087655f2
Latif Altay <latifaltay00@gmail.com>
2023-04-06 21:42:43 +0300

    06.04.2023_1

---
+++ 46-JDBC_Intro/jdbcIntro/src/DbHelper.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbHelper {
+    private String userName = "root";
+    private String password = "Deneme01!";
+    private String dbUrl = "jdbc:mysql://localhost:3306/world";
+
+    public Connection getConncetion() throws SQLException {
+        return DriverManager.getConnection(dbUrl,userName,password);
+    }
+
+
+    public void showErrorMessage(SQLException exception){
+        System.out.println("Error : " + exception);
+        System.out.println("Error code: " + exception.getErrorCode());
+    }
+
+
+}
--- 46-JDBC_Intro/jdbcIntro/src/Main.java
+++ 46-JDBC_Intro/jdbcIntro/src/Main.java
@@ -4,18 +4,16 @@ import java.sql.SQLException;
 
 public class Main {
 
-    static String userName = "root";
-    static String password = "Deneme01!";
-    static String dbUrl = "jdbc:mysql://localhost:3306/world";
     public static void main(String[] args) throws SQLException {
 
         Connection connection = null;
+        DbHelper helper = new DbHelper();
 
         try{
-           connection = DriverManager.getConnection(dbUrl,userName,password);
+           connection = helper.getConncetion();
             System.out.println("Bağlantı başarılı");
-        }catch (Exception exception){
-            System.out.println(exception.getMessage());
+        }catch (SQLException exception){
+            helper.showErrorMessage(exception);
         }finally {
             connection.close();
             System.out.println("Bağlantı bitti");
