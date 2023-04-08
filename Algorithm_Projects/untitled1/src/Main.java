c250b876982ca79db895445144a8b7ec4669f013
Latif Altay <latifaltay00@gmail.com>
2023-04-08 23:23:12 +0300

    08.04.2023_2

--- Algorithm_Projects/untitled1/src/Main.java
+++ Algorithm_Projects/untitled1/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int a = scan.nextInt();
+
+        if(a%2 == 0){
+            System.out.println("Sayı çifttir");
+        }else{
+            System.out.println("Sayı tektir");
+        }
+
     }
 }
