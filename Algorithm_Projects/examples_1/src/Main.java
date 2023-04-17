a13eac31c87286e4644bf9e2624c0d3c3e48330d
Latif Altay <latifaltay00@gmail.com>
2023-04-17 23:01:33 +0300

    17.04.2023_2

--- Algorithm_Projects/examples_1/src/Main.java
+++ Algorithm_Projects/examples_1/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç saat kalındı: ");
+        int saat = scan.nextInt();
+
+        double price = saat * 1.5;
+
+        System.out.println("ödemeniz gereken toplam fiyat: "+price);
+
     }
 }
