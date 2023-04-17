b51c1d644044a7324f9828c753909f79d863b1cd
Latif Altay <latifaltay00@gmail.com>
2023-04-17 22:59:14 +0300

    17.04.2023

---
+++ Algorithm_Projects/leetCodeExamples/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scan.nextInt();
+        for(int i = 0; i <= nums.length; i++){
+            System.out.println(nums[i]);
+            if(number == nums[i]){
+                return i;
+            }else{
+                return -1;
+            }
+
+    }
+        return number;
+    }
