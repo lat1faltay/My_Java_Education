dc170b6d1955cc00342c22853918ec3327d13738
Latif Altay <latifaltay00@gmail.com>
2023-04-17 23:05:10 +0300

    17.04.2023_2

--- Algorithm_Projects/examples_1/src/Main.java
+++ Algorithm_Projects/examples_1/src/Main.java
@@ -5,10 +5,17 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.print("Kaç saat kalındı: ");
         int saat = scan.nextInt();
+        System.out.println("Araç tipiniz nedir (1 küçük araç, 2 büyük araç) :");
+        int aracTipi = scan.nextInt();
+        double price = 0;
+        if (aracTipi == 1) {
+            price = saat * 1.5;
+        } else {
+            price = saat * 3;
+        }
 
-        double price = saat * 1.5;
+        System.out.println("ödemeniz gereken toplam fiyat: " + price);
 
-        System.out.println("ödemeniz gereken toplam fiyat: "+price);
 
     }
 }
