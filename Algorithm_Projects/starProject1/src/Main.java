a57354ca74f4dce503b0d74e7703fee6eeab00f0
Latif Altay <latifaltay00@gmail.com>
2023-04-18 23:22:27 +0300

    18.04.2023_2

--- Algorithm_Projects/starProject1/src/Main.java
+++ Algorithm_Projects/starProject1/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int n = scan.nextInt();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int k=2*n-1;k>=(2*i+1);k--){
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+
+
     }
 }
