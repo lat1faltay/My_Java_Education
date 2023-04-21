0a385dd9cbc38f602520834ef3ac51cbf1acb0cb
Latif Altay <latifaltay00@gmail.com>
2023-04-21 18:23:45 +0300

    updatedProject

--- Algorithm_Projects/ebob/src/Main.java
+++ Algorithm_Projects/ebob/src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int number = scan.nextInt();
 
-        
+        for(int i = 1; i <= number; i++){
+            if(number % i == 0){
+                System.out.println(i);
+            }
+        }
 
     }
 }
