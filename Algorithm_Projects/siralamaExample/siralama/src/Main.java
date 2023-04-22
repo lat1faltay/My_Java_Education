83e06156821a94a88a969efb82565f1b3d3c7945
Latif Altay <latifaltay00@gmail.com>
2023-04-22 21:14:38 +0300

    sıralamaOrnegi_updated

--- Algorithm_Projects/siralamaExample/siralama/src/Main.java
+++ Algorithm_Projects/siralamaExample/siralama/src/Main.java
@@ -1,5 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] data = {1,2,46,6,8,90,5,345,67,89,4};
+        for(int i = 0; i< data.length; i++){
+            System.out.println(data[i]);
+        }
+        Arrays.sort(data);
+
+        // sıralanmış liste
+        System.out.println("1. sıralama tantiği");
+        for(int num : data){
+            System.out.println(num);
+        }
         
     }
 }
