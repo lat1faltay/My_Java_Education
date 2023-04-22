733e9fef0ac400d09f8d204aa826cdcd38a91a7d
Latif Altay <latifaltay00@gmail.com>
2023-04-22 21:18:05 +0300

    sıralamaOrnegi_updated2

--- Algorithm_Projects/siralamaExample/siralama/src/Main.java
+++ Algorithm_Projects/siralamaExample/siralama/src/Main.java
@@ -1,19 +1,39 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] data = {1,2,46,6,8,90,5,345,67,89,4};
-        for(int i = 0; i< data.length; i++){
+        int[] data = {1, 2, 46, 6, 8, 90, 5, 345, 67, 89, 4};
+        for (int i = 0; i < data.length; i++) {
             System.out.println(data[i]);
         }
-        Arrays.sort(data);
-
+        System.out.println();
+        System.out.println();
         // sıralanmış liste
-        System.out.println("1. sıralama tantiği");
-        for(int num : data){
+        Arrays.sort(data);
+        System.out.println("1. sıralama taktiği");
+        for (int num : data) {
             System.out.println(num);
         }
-        
+
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[i] > data[j]) {
+                    int temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                }
+            }
+        }
+
+        // Sıralanmış tablo yazdırılıyor
+        System.out.println("Sıralanmış tablo:");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+
+
     }
 }
