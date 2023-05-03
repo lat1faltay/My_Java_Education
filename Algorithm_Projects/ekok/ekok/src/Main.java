8cf000a09844cde86dbbf3b87ed6aae03a467878
Latif Altay <latifaltay00@gmail.com>
2023-05-03 20:28:04 +0300

    project ended

--- Algorithm_Projects/ekok/ekok/src/Main.java
+++ Algorithm_Projects/ekok/ekok/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int n1 = 48, n2 = 160, ekok;
+        ekok= (n1 > n2) ? n1 : n2;
+        while(true)
+        {
+            if( ekok % n1 == 0 && ekok % n2 == 0 )
+            {
+                System.out.printf("%d ve %d sayılarının EKOK'u %d dir.", n1, n2, ekok);
+                break;
+            }
+            ++ekok;
+        }
     }
 }
