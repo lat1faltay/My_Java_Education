0efee74a2da2a5042f8c62b111f99e0423a4d032
Latif Altay <latifaltay00@gmail.com>
2023-05-22 20:12:19 +0300

    project is complated

--- Algorithm_Projects/newProject/bodyIndex/src/Main.java
+++ Algorithm_Projects/newProject/bodyIndex/src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Boyunuzu girin(cm) : ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Kilonuzu girin(kg) : ");
+        double k = scanner.nextDouble();
+
+
+        b=b/100;
+        double endeks  = k/(b*b);
+
+        System.out.println(b);
+        System.out.println(k);
+        if(endeks <18)
+        {
+            System.out.print(String.format("ZAYIF vki: %f", endeks));
+        }
+        else if(endeks >= 18 && endeks <25)
+        {
+            System.out.print(String.format("NORMAL vki %f", endeks));
+        }
+        else if(endeks >= 25 && endeks <30)
+        {
+            System.out.print(String.format("KİLOLU vki: %f", endeks));
+        }
+        else if(endeks >= 30 && endeks <35)
+        {
+            System.out.print(String.format("OBEZ vki: %f", endeks));
+        }
+        else
+        {
+            System.out.print(String.format("CİDDİ OBEZ vki: %f", endeks));
+        }
     }
 }
